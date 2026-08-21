07e081c4143ff2649a688a15fa4ad85081438646
MatheusCristophi <matheuscr909@gmail.com>
2026-08-21 19:32:45 -0300

    feat: adiciona a lógica de criação de usuário

--- src/main/java/com/finance/manager/controladores/usuariodtos/UsuarioRequisicao.java
+++ src/main/java/com/finance/manager/controladores/usuariodtos/UsuarioRequisicao.java
@@ -1,11 +1,8 @@
 package com.finance.manager.controladores.usuariodtos;
 
-import com.finance.manager.enums.UsuarioRoles;
-
 public record UsuarioRequisicao(
         String nome,
         String email,
-        String senha,
-        UsuarioRoles role
+        String senha
 ) {
 }
--- src/main/java/com/finance/manager/servicos/UsuarioServico.java
+++ src/main/java/com/finance/manager/servicos/UsuarioServico.java
@@ -1,20 +1,39 @@
 package com.finance.manager.servicos;
 
+import com.finance.manager.controladores.usuariodtos.UsuarioRequisicao;
+import com.finance.manager.controladores.usuariodtos.UsuarioResposta;
+import com.finance.manager.entidades.Empresas;
 import com.finance.manager.entidades.Usuario;
 import com.finance.manager.entidades.UsuarioAutenticado;
+import com.finance.manager.entidades.UsuarioEmpresa;
+import com.finance.manager.enums.UsuarioRoles;
+import com.finance.manager.excecoes.EmailJaExisteException;
 import com.finance.manager.excecoes.NaoEncontradoException;
+import com.finance.manager.repositorios.EmpresaRepositorio;
+import com.finance.manager.repositorios.UsuarioEmpresaRepositorio;
 import com.finance.manager.repositorios.UsuarioRepositorio;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 public class UsuarioServico implements UserDetailsService {
-    private final UsuarioRepositorio usuarioRepositorio;
 
-    public UsuarioServico(UsuarioRepositorio usuarioRepositorio) {
+    private final UsuarioRepositorio usuarioRepositorio;
+    private final EmpresaRepositorio empresaRepositorio;
+    private final UsuarioEmpresaRepositorio usuarioEmpresaRepositorio;
+    private final BCryptPasswordEncoder encoder;
+
+    public UsuarioServico(UsuarioRepositorio usuarioRepositorio, EmpresaRepositorio empresaRepositorio, UsuarioEmpresaRepositorio usuarioEmpresaRepositorio, BCryptPasswordEncoder encoder) {
         this.usuarioRepositorio = usuarioRepositorio;
+        this.empresaRepositorio = empresaRepositorio;
+        this.usuarioEmpresaRepositorio = usuarioEmpresaRepositorio;
+        this.encoder = encoder;
     }
 
     @Override
@@ -23,5 +42,34 @@ public class UsuarioServico implements UserDetailsService {
                 .orElseThrow(() -> new NaoEncontradoException("O Usuário"));
 
         return new UsuarioAutenticado(usuario);
+    }
+
+    @Transactional
+    public UsuarioResposta criarUsuario(UsuarioRequisicao requisicao,
+                                        UsuarioRoles role,
+                                        UUID empresaId
+    ) {
+        Usuario usuario = new Usuario();
+
+        if(usuarioRepositorio.findByEmail(requisicao.email()).isPresent()) {
+            throw new EmailJaExisteException(requisicao.email());
+        }
+
+        usuario.setNome(requisicao.nome());
+        usuario.setEmail(requisicao.email().toLowerCase());
+        usuario.setSenha(encoder.encode(requisicao.senha()));
+        usuarioRepositorio.save(usuario);
+
+        Empresas empresa = empresaRepositorio.findById(empresaId)
+                .orElseThrow(() -> new NaoEncontradoException("a Empresa"));
+
+        UsuarioEmpresa usuarioEmpresa = new UsuarioEmpresa();
+
+        usuarioEmpresa.setUsuarioId(usuario);
+        usuarioEmpresa.setEmpresaId(empresa);
+        usuarioEmpresa.setRole(role);
+        usuarioEmpresaRepositorio.save(usuarioEmpresa);
+
+        return UsuarioResposta.from(usuario);
     }
 }
