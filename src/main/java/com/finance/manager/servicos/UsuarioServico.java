aea6feb250e529fc2d86951f7106ce9e2ae77548
MatheusCristophi <matheuscr909@gmail.com>
2026-08-21 21:30:52 -0300

    feat: adiciona a lógica para buscar todos os usuários caso seja o dono ou o administrador

--- src/main/java/com/finance/manager/servicos/UsuarioServico.java
+++ src/main/java/com/finance/manager/servicos/UsuarioServico.java
@@ -9,6 +9,7 @@ import com.finance.manager.entidades.UsuarioEmpresa;
 import com.finance.manager.enums.UsuarioRoles;
 import com.finance.manager.excecoes.EmailJaExisteException;
 import com.finance.manager.excecoes.NaoEncontradoException;
+import com.finance.manager.excecoes.SemPermissaoException;
 import com.finance.manager.repositorios.EmpresaRepositorio;
 import com.finance.manager.repositorios.UsuarioEmpresaRepositorio;
 import com.finance.manager.repositorios.UsuarioRepositorio;
@@ -19,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -42,6 +44,20 @@ public class UsuarioServico implements UserDetailsService {
                 .orElseThrow(() -> new NaoEncontradoException("O Usuário"));
 
         return new UsuarioAutenticado(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public List<UsuarioResposta> buscarUsuarios(UUID usuarioId){
+        Usuario usuario = usuarioRepositorio.findById(usuarioId)
+                .orElseThrow(() -> new SemPermissaoException(usuarioId.toString()));
+
+        UsuarioEmpresa usuarioEmpresa = usuarioEmpresaRepositorio.findByUsuarioId(usuario);
+
+        if(usuarioEmpresa.getRole() != UsuarioRoles.DONO || usuarioEmpresa.getRole() != UsuarioRoles.ADMINISTRADOR_DO_SISTEMA)
+            throw new SemPermissaoException(usuario.getEmail());
+
+        List<Usuario> usuarios = usuarioRepositorio.findAll();
+        return UsuarioResposta.of(usuarios);
     }
 
     @Transactional
