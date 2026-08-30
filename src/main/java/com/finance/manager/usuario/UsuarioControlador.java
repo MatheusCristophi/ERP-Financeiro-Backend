f53cba899522f670fb0dcf1d2268f4bbf2a9e481
MatheusCristophi <matheuscr909@gmail.com>
2026-08-30 00:08:19 -0300

    feat: adiciona endpoint para buscar geral dos usuários da empresa

--- src/main/java/com/finance/manager/usuario/UsuarioControlador.java
+++ src/main/java/com/finance/manager/usuario/UsuarioControlador.java
@@ -1,9 +1,31 @@
 package com.finance.manager.usuario;
 
+import com.finance.manager.usuario.dto.UsuarioResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioControlador {
+
+    private final UsuarioServico usuarioServico;
+
+    public UsuarioControlador(UsuarioServico usuarioServico) {
+        this.usuarioServico = usuarioServico;
+    }
+
+    @GetMapping("/{empresaId}")
+    public ResponseEntity<List<UsuarioResposta>> buscarUsuarios(@AuthenticationPrincipal Usuario usuario,
+                                                                @PathVariable UUID empresaId) {
+        List<UsuarioResposta> resposta = usuarioServico.buscarUsuarios(usuario.getUsuarioId(), empresaId);
+        return new ResponseEntity<>(resposta, HttpStatus.OK);
+    }
 }
