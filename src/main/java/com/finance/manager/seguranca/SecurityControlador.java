8dba764be0c510aaf5abf05e031109b4fa192dea
MatheusCristophi <matheuscr909@gmail.com>
2026-08-31 00:42:08 -0300

    feat: adiciona endpoint de login para o sistema

---
+++ src/main/java/com/finance/manager/seguranca/SecurityControlador.java
@@ -0,0 +1,26 @@
+package com.finance.manager.seguranca;
+
+import com.finance.manager.seguranca.dtos.SecurityRequisicao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class SecurityControlador {
+
+    private final SecurityServico securityServico;
+
+    public SecurityControlador(SecurityServico securityServico) {
+        this.securityServico = securityServico;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> logar(@RequestBody SecurityRequisicao requisicao) {
+        String token = securityServico.logar(requisicao);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+}
