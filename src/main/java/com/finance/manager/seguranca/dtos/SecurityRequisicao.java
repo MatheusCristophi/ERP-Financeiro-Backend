679e8e98f7225eec50c81c416f80a94b6e64d205
MatheusCristophi <matheuscr909@gmail.com>
2026-08-30 22:46:44 -0300

    feat: adiciona dto de requisição ra login

---
+++ src/main/java/com/finance/manager/seguranca/dtos/SecurityRequisicao.java
@@ -0,0 +1,6 @@
+package com.finance.manager.seguranca.dtos;
+
+public record SecurityRequisicao(String nome,
+                                 String email,
+                                 String senha) {
+}
