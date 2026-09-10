6e280557035483d8753a8986be827e6c08f9e2a0
MatheusCristophi <matheuscr909@gmail.com>
2026-09-09 23:01:04 -0300

    feat: adiciona endpoint para buscar Categoria pelo id

--- src/main/java/com/finance/manager/categoria/CategoriaControlador.java
+++ src/main/java/com/finance/manager/categoria/CategoriaControlador.java
@@ -5,10 +5,7 @@ import com.finance.manager.usuario.Usuario;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,6 +23,12 @@ public class CategoriaControlador {
     @GetMapping
     public ResponseEntity<List<CategoriaResposta>> buscarTodasCategorias(@AuthenticationPrincipal Usuario usuario, @RequestParam UUID empresaId) {
         List<CategoriaResposta> resposta = categoriaServico.buscarTodasCategorias(empresaId, usuario.getUsuarioId());
-        return new ResponseEntity<>(resposta, HttpStatus.OK);
+        return ResponseEntity.ok(resposta);
+    }
+
+    @GetMapping("{categoriaId}")
+    public ResponseEntity<CategoriaResposta> buscarCategoria(@AuthenticationPrincipal Usuario usuario, @RequestParam UUID empresaId, @PathVariable UUID categoriaId) {
+        CategoriaResposta resposta = categoriaServico.buscarCategoria(empresaId, usuario.getUsuarioId(), categoriaId);
+        return ResponseEntity.ok(resposta);
     }
 }
