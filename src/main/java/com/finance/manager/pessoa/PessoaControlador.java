c474df23f42a161fd86f9f4163b74c3b9a1c8256
MatheusCristophi <matheuscr909@gmail.com>
2026-09-22 20:15:48 -0300

    feat: adiciona endpoint para criar pessoa no sistema

---
+++ src/main/java/com/finance/manager/pessoa/PessoaControlador.java
@@ -0,0 +1,31 @@
+package com.finance.manager.pessoa;
+
+import com.finance.manager.pessoa.dto.PessoaRequisicao;
+import com.finance.manager.pessoa.dto.PessoaResposta;
+import com.finance.manager.seguranca.UsuarioAutenticado;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("pessoas")
+public class PessoaControlador {
+
+    private final PessoaServico pessoaServico;
+
+    public PessoaControlador(PessoaServico pessoaServico) {
+        this.pessoaServico = pessoaServico;
+    }
+
+    @PostMapping("/{empresaId}")
+    public ResponseEntity<PessoaResposta> criarPessoa(@AuthenticationPrincipal UsuarioAutenticado usuario,
+                                                      @PathVariable UUID empresaId,
+                                                      @RequestBody PessoaRequisicao requisicao) {
+        PessoaResposta resposta = pessoaServico.criarPessoa(usuario.getUsuario().getId(), empresaId, requisicao);
+        return new ResponseEntity<>(resposta, HttpStatus.CREATED);
+    }
+}
