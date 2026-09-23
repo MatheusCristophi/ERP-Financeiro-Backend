46a59938c7221c2773f2fdfc226bb5030ae784a7
MatheusCristophi <matheuscr909@gmail.com>
2026-09-23 01:12:54 -0300

    feat: adiciona método de criação de lançamentos no sistema

--- src/main/java/com/finance/manager/lancamento/LancamentoServico.java
+++ src/main/java/com/finance/manager/lancamento/LancamentoServico.java
@@ -1,15 +1,21 @@
 package com.finance.manager.lancamento;
 
+import com.finance.manager.categoria.Categoria;
+import com.finance.manager.categoria.CategoriaRepositorio;
 import com.finance.manager.empresa.EmpresaRepositorio;
 import com.finance.manager.empresa.Empresas;
 import com.finance.manager.excecoes.NaoEncontradoException;
 import com.finance.manager.excecoes.SemPermissaoException;
 import com.finance.manager.excecoes.VinculoNaoEncontrado;
+import com.finance.manager.lancamento.dto.LancamentoRequisicao;
 import com.finance.manager.lancamento.dto.LancamentoResposta;
+import com.finance.manager.pessoa.Pessoa;
+import com.finance.manager.pessoa.PessoaRepositorio;
 import com.finance.manager.usuario.Usuario;
 import com.finance.manager.usuario.UsuarioRepositorio;
 import com.finance.manager.usuarioempresa.UsuarioEmpresa;
 import com.finance.manager.usuarioempresa.UsuarioEmpresaRepositorio;
+import com.finance.manager.usuarioempresa.UsuarioRoles;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,12 +28,58 @@ public class LancamentoServico {
     private final UsuarioRepositorio usuarioRepositorio;
     private final EmpresaRepositorio empresaRepositorio;
     private final UsuarioEmpresaRepositorio usuarioEmpresaRepositorio;
+    private final CategoriaRepositorio categoriaRepositorio;
+    private final PessoaRepositorio pessoaRepositorio;
 
-    public LancamentoServico(LancamentoRepositorio lancamentoRepositorio, UsuarioRepositorio usuarioRepositorio, EmpresaRepositorio empresaRepositorio, UsuarioEmpresaRepositorio usuarioEmpresaRepositorio) {
+    public LancamentoServico(LancamentoRepositorio lancamentoRepositorio, UsuarioRepositorio usuarioRepositorio, EmpresaRepositorio empresaRepositorio, UsuarioEmpresaRepositorio usuarioEmpresaRepositorio, CategoriaRepositorio categoriaRepositorio, PessoaRepositorio pessoaRepositorio) {
         this.lancamentoRepositorio = lancamentoRepositorio;
         this.usuarioRepositorio = usuarioRepositorio;
         this.empresaRepositorio = empresaRepositorio;
         this.usuarioEmpresaRepositorio = usuarioEmpresaRepositorio;
+        this.categoriaRepositorio = categoriaRepositorio;
+        this.pessoaRepositorio = pessoaRepositorio;
+    }
+
+    public LancamentoResposta criarLancamentos(UUID usuarioId, UUID empresaId, UUID categoriaId, UUID pessoaId, LancamentoRequisicao requisicao) {
+        Usuario usuario = usuarioRepositorio.findById(usuarioId)
+                .orElseThrow(() -> new NaoEncontradoException("o Usuário"));
+
+        Empresas empresas = empresaRepositorio.findById(empresaId)
+                .orElseThrow(() -> new NaoEncontradoException("a Empresa"));
+
+        UsuarioEmpresa vinculo = usuarioEmpresaRepositorio.findByUsuarioIdAndEmpresaId(usuario.getId(), empresas.getId())
+                .orElseThrow(() -> new VinculoNaoEncontrado(usuarioId, empresaId));
+
+        if(vinculo.getRole() == UsuarioRoles.CONSULTOR) throw new SemPermissaoException(usuario.getNome());
+
+        Categoria categoria = categoriaRepositorio.findById(categoriaId)
+                .orElseThrow(() -> new NaoEncontradoException("a Categoria"));
+
+        if(!categoria.getCategoriaEmpresa().equals(empresas)) throw new NaoEncontradoException("a Categoria");
+
+        Pessoa pessoa = pessoaRepositorio.findById(pessoaId)
+                .orElseThrow(() -> new NaoEncontradoException("a Pessoa"));
+
+        if(!pessoa.getPessoaEmpresa().equals(empresas)) throw new NaoEncontradoException("a Pessoa");
+
+        Lancamento lancamento = new Lancamento();
+
+        lancamento.setDescricao(requisicao.descricao());
+        lancamento.setValor(requisicao.valor());
+        lancamento.setDataEmissao(requisicao.dataEmissao());
+        lancamento.setDataVencimento(requisicao.dataVencimento());
+        lancamento.setDataPagamento(requisicao.dataPagamento());
+        lancamento.setLancamentoCategoria(categoria);
+        lancamento.setLancamentoEmpresa(empresas);
+        lancamento.setLancamentoPessoa(pessoa);
+        lancamento.setLancamentoUsuario(usuario);
+        lancamento.setMovimentacao(requisicao.movimentacaoTipo());
+        lancamento.setStatus(requisicao.status());
+        lancamento.setObservacao(requisicao.observacao());
+
+        lancamentoRepositorio.save(lancamento);
+
+        return LancamentoResposta.from(lancamento);
     }
 
     public List<LancamentoResposta> buscarTodosLancamentos(UUID usuarioId, UUID empresaId) {
